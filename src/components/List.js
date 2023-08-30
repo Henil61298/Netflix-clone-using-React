201957ee9cc2fd537cdaaaa371fc9df1ae4ed00b
Henil Shah <henil@Henils-MacBook-Pro.local>
2023-08-31 01:49:11 +0530

    Adding routes

---
+++ src/components/List.js
@@ -0,0 +1,45 @@
+const List = () => {
+    return (
+        <div className="list">
+      <div className="row">
+        <h2 className="text-white title">Netflix</h2>
+        <div className="col">
+          <div className="row__posters">
+              <img
+                className="row__poster row__posterLarge"
+                src="https://www.themoviedb.org/t/p/w440_and_h660_face/1FCsjYtEtuine1TUssIFjy6vmJW.jpg"
+                alt=""
+              />
+              <img
+                className="row__poster row__posterLarge"
+                src="https://www.themoviedb.org/t/p/w440_and_h660_face/1FCsjYtEtuine1TUssIFjy6vmJW.jpg"
+                alt=""
+              />
+              <img
+                className="row__poster row__posterLarge"
+                src="https://www.themoviedb.org/t/p/w440_and_h660_face/1FCsjYtEtuine1TUssIFjy6vmJW.jpg"
+                alt=""
+              />
+              <img
+                className="row__poster row__posterLarge"
+                src="https://www.themoviedb.org/t/p/w440_and_h660_face/1FCsjYtEtuine1TUssIFjy6vmJW.jpg"
+                alt=""
+              />
+              <img
+                className="row__poster row__posterLarge"
+                src="https://www.themoviedb.org/t/p/w440_and_h660_face/1FCsjYtEtuine1TUssIFjy6vmJW.jpg"
+                alt=""
+              />
+              <img
+                className="row__poster row__posterLarge"
+                src="https://www.themoviedb.org/t/p/w440_and_h660_face/1FCsjYtEtuine1TUssIFjy6vmJW.jpg"
+                alt=""
+              />
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default List;
